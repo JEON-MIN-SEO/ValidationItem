314959915e1f0d8971cd1d9bc91d26a253d2131e
JEON-MIN-SEO <jmsparrowk@naver.com>
2024-04-18 20:21:15 +0900

    4/18

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -3,9 +3,13 @@ package hello.itemservice.web.validation;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -13,6 +17,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @Controller
 @RequestMapping("/validation/v2/items")
 @RequiredArgsConstructor
@@ -41,29 +46,28 @@ public class ValidationItemControllerV2 {
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
+    public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
-        Map<String, String> errors = new HashMap<>();
 
         if(!StringUtils.hasText(item.getItemName())){
-            errors.put("ItemName","상품 이름은 필수입니다.");
+            bindingResult.addError(new FieldError("item", "ItemName","상품 이름은 필수입니다."));
         }
         if (item.getPrice() == null || item.getPrice()<1000 || item.getPrice()>1000000){
-            errors.put("Price", "가격은 1,000~1,000,000 까지 허용합니다");
+            bindingResult.addError(new FieldError("item", "Price", "가격은 1,000~1,000,000 까지 허용합니다"));
         }
         if (item.getQuantity() == null || item.getQuantity() > 9999) {
-            errors.put("Quantity", "수량은 최대 9999 까지 허용합니다.");
+            bindingResult.addError(new FieldError("item", "Quantity", "수량은 최대 9999 까지 허용합니다."));
         }
 
         if(item.getPrice() != null && item.getQuantity()!=null){
             int result = item.getPrice() * item.getQuantity();
             if(result < 10000){
-                errors.put("globalError", "가격 * 수량의 값이 10,000원 이상이어야 합니다. 현재값="+result);
+                bindingResult.addError(new ObjectError("item", "가격 * 수량의 값이 10,000원 이상이어야 합니다. 현재값="+result));
             }
         }
 
-        if(!errors.isEmpty()){
-            model.addAttribute("errors", errors);
+        if(bindingResult.hasErrors()){
+            log.info("errors={}",bindingResult);
             return "validation/v2/addForm";
         }
 
